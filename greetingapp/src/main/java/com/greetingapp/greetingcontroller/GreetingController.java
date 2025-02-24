66cd830224ebfe32a8ad763da6ebfb348bcc60e2
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-02-24 10:26:21 +0530

    [Pranav] Add. GreetingController for different HTTP Methods.

---
+++ greetingapp/src/main/java/com/greetingapp/greetingcontroller/GreetingController.java
@@ -0,0 +1,45 @@
+package com.greetingapp.greetingcontroller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    // GET request
+    @GetMapping
+    public Map<String, String> getGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, World! (GET)");
+        return response;
+    }
+
+    // POST request
+    @PostMapping
+    public Map<String, String> postGreeting(@RequestBody Map<String, String> request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, " + request.get("name") + "! (POST)");
+        return response;
+    }
+
+    // PUT request
+    @PutMapping("/{id}")
+    public Map<String, String> putGreeting(@PathVariable String id, @RequestBody Map<String, String> request) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Updated greeting with ID: " + id + " to: " + request.get("name") + " (PUT)");
+        return response;
+    }
+
+    // DELETE request
+    @DeleteMapping("/{id}")
+    public Map<String, String> deleteGreeting(@PathVariable String id) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Deleted greeting with ID: " + id + " (DELETE)");
+        return response;
+    }
+}
+
